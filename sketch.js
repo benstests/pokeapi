abdf7c019bae1ce1f846711f0a89d10562c24952
benstests <95652162+benstests@users.noreply.github.com>
2021-12-25 00:09:13 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ let pokeList = [];
 let dataCount = 1;
 
 function preload() {
-  for (let i = 1; i < 10; i++) {
+  for (let i = 1; i < 500; i++) {
     let link = "https://pokeapi.co/api/v2/pokemon/" + i;
     loadJSON(link, gotData);
   }
@@ -33,6 +33,18 @@ function draw() {
   //draws battle
   background('#B5FFE4');
   noStroke();
+  fill('#E1C7AA');
+  ellipse(450, 145, 230, 80);
+  fill('#3DE87B');
+  ellipse(450, 145, 210, 60);
+  pokeList[100].drawEnemy();
+  pokeList[2].active = true;
+  fill('#E1C7AA');
+  ellipse(150, 360, 230, 80);
+  fill('#3DE87B');
+  ellipse(150, 360, 210, 60);
+  pokeList[2].drawActive();
+}
   fill('#E1C7AA');
   ellipse(450, 145, 230, 80);
   fill('#3DE87B');
